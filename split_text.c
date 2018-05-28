a87c2216b9cf3d64bd9b948ca99e6a9b6abece25
すー <aaaiiiuuu35@gmail.com>
2018-05-29 02:35:04 +0900

    Update split_text.c

--- split_text.c
+++ split_text.c
@@ -21,3 +21,26 @@ int main(void){
 }   
 
 //int printf(const char * restrict, ...) __attribute__((__format__ (__printf__, 1, 2)));
+/*
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLEN 64
+
+int main(void){
+	char s1[]="Network";
+    char s2[MAXLEN],s3[MAXLEN];
+	int i;
+
+    for(i=0;s1[i]!='\0';i++){
+        s2[i]=s1[i];
+    }
+	s2[i]=s1[i];
+    
+    strcpy(s3,s1);
+
+    if(!(strcmp(s2,s3)))printf("s2=s3\n");
+	
+	return 0;
+}
+*/
