46f780978e533dd82cd4188476de39083009548b
すー <aaaiiiuuu35@gmail.com>
2018-11-17 12:06:57 +0900

    repot + sort

---
+++ 4s/report/src/Paint.java
@@ -0,0 +1,137 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.awt.Color;
+import java.awt.Graphics;
+
+class Paint extends Frame implements MouseListener, MouseMotionListener{
+	int x, y;
+	ArrayList<Figure> objList;
+	Figure obj;
+	public static void main(String[] args){
+		Paint f = new Paint();
+		f.setSize(640, 480);
+		f.setTitle("Paint Sample");
+		f.addWindowListener(new WindowAdapter(){
+			@Override public void windowClosing(WindowEvent e){
+				System.exit(0);
+			}});
+		f.setVisible(true);
+	}
+
+	Paint(){
+		objList = new ArrayList<Figure>();
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
+
+	@Override public void paint(Graphics g){
+		Figure f;
+		for(int i = 0;i < objList.size();i++){
+			f = objList.get(i);
+			f.paint(g);
+		}
+		if(obj != null) obj.paint(g);
+	}
+
+	@Override public void mousePressed(MouseEvent e){
+		x = e.getX();
+		y = e.getY();
+		obj = new Circle();
+		obj.moveto(x,y);
+		objList.add(obj);
+		obj = null;
+		repaint();
+	}
+	@Override public void mouseReleased(MouseEvent e){}
+	@Override public void mouseClicked(MouseEvent e){
+		x = e.getX();
+		y = e.getY();
+		if(obj != null)obj.moveto(x,y);
+		repaint();
+	}
+	@Override public void mouseEntered(MouseEvent e){}
+	@Override public void mouseExited(MouseEvent e){}
+	@Override public void mouseDragged(MouseEvent e){
+
+	}
+
+//	@Override public void update(Graphics g){
+//		paint(g);
+//	}
+	@Override public void mouseMoved(MouseEvent e){}	
+}
+
+class Coord {
+	int x, y;
+	Coord(){
+		x = y = 0;
+	}
+	Coord(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public void move(int dx, int dy){
+		this.x += dx;
+		this.y += dy;
+	}
+
+	public void moveto(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	public void paint(Graphics g){}
+}
+
+class Figure extends Coord {
+	int color;
+	Figure(){
+		this.color = 0;
+	}
+	Figure(int x, int y){
+		super(x,y);
+		this.color = 0;
+	}
+}
+
+
+class Circle extends Figure {
+	int color, size;
+	int color_flag;
+	
+	Circle(){
+		color = 0;
+		size = 30;
+	}
+	
+	Circle(int x, int y){
+		super(x, y);
+		this.size = 30;
+	}
+
+	@Override public void paint(Graphics g){
+		Random rnd = new Random();
+		Color col = new Color(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255));
+		g.setColor(col);
+		g.fillOval(x - size / 2, y - size / 2, size, size);
+	}
+}
+
+class Box extends Coord {
+	int color, size;
+	
+	Box(){
+		color = 0;
+		size = 30;
+	}
+	
+	Box(int x, int y){
+		super(x, y);
+		this.size = 5000;
+	}
+	@Override public void paint(Graphics g){
+		g.fillRect(x - size / 2, y - size / 2, size, size);
+	}
+}
