7034d116ec9162e7c286bd8b337b08fce40c8556
すー <aaaiiiuuu35@gmail.com>
2018-12-03 21:24:39 +0900

    4ｓ

--- 4s/report/src/Paint.java
+++ 4s/report/src/Paint.java
@@ -1,25 +1,33 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.Random;
 import java.awt.Color;
 import java.awt.Graphics;
 
 class Paint extends Frame implements MouseListener, MouseMotionListener{
 
-	int x, y;
-	ArrayList<Figure> objList;
-	Figure obj;
+	private int x, y;
+	private ArrayList<Figure> objList;
+	private Figure obj;
+	private static int visibleCount = 0;
+	private boolean isDoragge = false;
+	private Figure PressBuffer = null;
 
 	public static void main(String[] args){
 		Paint f = new Paint();
-		f.setSize(1280, 720);
-		f.setTitle("Paint Sample");
+//		f.setSize(1280, 720);
+		f.setBounds(500, 500, 1920, 1080);
+		f.setTitle("Paint");
+		//f.setBackground(Color.BLACK);
 		f.addWindowListener(new WindowAdapter(){
 			@Override public void windowClosing(WindowEvent e){
 				System.exit(0);
 			}});
 		f.setVisible(true);
+		if(args.length != 0){
+			Paint.visibleCount = Integer.parseInt(args[0]);
+		}
+		if(visibleCount < 0)Paint.visibleCount = 0;
 	}
 
 	Paint() {
@@ -32,8 +40,11 @@ class Paint extends Frame implements MouseListener, MouseMotionListener{
 		Figure f;
 		Image back = back = createImage(getSize().width, getSize().height);
 		Graphics buffer = back.getGraphics();
-
-		for(int i = 0;i < objList.size();i++){
+		int start = 0;
+		if(objList.size() >= Paint.visibleCount && Paint.visibleCount != 0){
+			start = objList.size() - Paint.visibleCount;
+		}
+		for(int i = start;i < objList.size();i++){
 			f = objList.get(i);
 			f.paint(buffer);
 		}
@@ -43,46 +54,63 @@ class Paint extends Frame implements MouseListener, MouseMotionListener{
 	}
 
 	@Override public void mousePressed(MouseEvent e){
+		this.isDoragge = false;
 		x = e.getX();
 		y = e.getY();
 		obj = new Circle();
 		obj.moveto(x,y);
+		PressBuffer = obj;
 		repaint();
 	}
 	@Override public void mouseReleased(MouseEvent e){
 		x = e.getX();
 		y = e.getY();
-		obj.moveto(x,y);
+		if(isDoragge == false){
+			obj = this.PressBuffer;
+		}else {
+			obj = new Circle();
+			obj.moveto(x,y);
+		}
 		objList.add(obj);
+		obj = null;
 		repaint();
-		System.out.println(this.objList.size());
+		System.out.println("count\t\t:" + this.objList.size());
 	}
 	@Override public void mouseClicked(MouseEvent e){
-		x = e.getX();
-		y = e.getY();
-		if(obj != null)obj.moveto(x,y);
+		int btn = e.getButton();
+		if (btn == MouseEvent.BUTTON3){
+			objList.clear();
+		}
+		System.out.println("--clear--");
 	}
 	@Override public void mouseEntered(MouseEvent e){}
 	@Override public void mouseExited(MouseEvent e){}
 	@Override public void mouseDragged(MouseEvent e){
 		x = e.getX();
 		y = e.getY();
-		obj = new Circle();
-		obj.moveto(x,y);
+		if(this.isDoragge == false){
+			obj = this.PressBuffer;
+		}else {
+			obj = new Circle();
+			obj.moveto(x,y);
+		}
+		this.isDoragge = true;
 		objList.add(obj);
 		obj = null;
 		repaint();
+		System.out.println("count\t\t:" + this.objList.size());
 	}
 
 	@Override public void update(Graphics g){
 		paint(g);
 	}
 
-	@Override public void mouseMoved(MouseEvent e){}	
+	@Override public void mouseMoved(MouseEvent e){}
+
 }
 
 class Coord {
-	int x, y;
+	protected int x, y;
 	Coord(){
 		x = y = 0;
 	}
@@ -104,31 +132,49 @@ class Coord {
 }
 
 class Figure extends Coord {
-	int color;
+	protected Color color;
+	private static float hue = 0;
+	private static final float saturation = 1.0f,brightness = 1.0f;
+	private static final float DIFF = (1.0f / 360);
+
 	Figure(){
-		this.color = 0;
+		this.color = Color.getHSBColor(Figure.hue, Figure.saturation, Figure.brightness);
+		this.NextColor();
 	}
+
 	Figure(int x, int y){
 		super(x,y);
-		this.color = 0;
+		this.color = Color.getHSBColor(Figure.hue, Figure.saturation, Figure.brightness);
+		this.NextColor();
+	}
+
+	void NextColor(){
+		System.out.println("hue\t\t:" + Figure.hue);
+		System.out.println("saturation\t:" + Figure.saturation);
+		System.out.println("brightness\t:" + Figure.brightness);
+		Figure.hue += this.DIFF;
+		if(Figure.hue > 1.0f)Figure.hue = 0.0f;
 	}
 }
 
 
 class Circle extends Figure {
-	int size;
-	int color_flag;
-	Color color;
+	private int size;
+	private static boolean size_flag = false;;
 
-	Circle(){
-		size = 30;
-		Random rnd = new Random();
-		this.color = new Color(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255));
+	Circle() {
+		if (Circle.size_flag == true) {
+			this.size = 60;
+			Circle.size_flag = false;
+		} else {
+			this.size = 30;
+			Circle.size_flag = true;
+		}
+		this.size = 30;
 	}
-	
+
 	Circle(int x, int y){
 		super(x, y);
-		this.size = 30;
 	}
 
 	@Override public void paint(Graphics g){
@@ -137,21 +183,3 @@ class Circle extends Figure {
 	}
 }
 
-class Box extends Coord {
-	int color, size;
-	
-	Box(){
-		color = 0;
-		size = 30;
-	}
-	
-	Box(int x, int y){
-		super(x, y);
-		this.size = 5000;
-	}
-	@Override public void paint(Graphics g){
-
-
-		g.fillRect(x - size / 2, y - size / 2, size, size);
-	}
-}
