29a72d3d8af89f720b62ae0276af2f0a281fec9a
すー <aaaiiiuuu35@gmail.com>
2019-05-27 23:40:58 +0900

    Update Cards class

--- 5s/Poker/Poker/Cards.cs
+++ 5s/Poker/Poker/Cards.cs
@@ -1,10 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Poker
 {
     class Cards
     {
+        private List<Card> cards;
+
+        public Cards()
+        {
+        }
+
+        public void AddCard(int _num) => cards.Add(new Card(_num));
+        public void AddCard(params int[] _num)
+        {
+            foreach(var i in Enumerable.Range(0, _num.Length))
+            {
+                cards.Add(new Card(_num[i]));
+            }
+        }
+
+        public Card this[int index] {
+            get {
+                return cards[index];
+            }
+        }
+
+        public int GetCardNumber(int index) => cards[index].Number;
+
+        public int GetSize => this.cards.Count;
+
+        public Card GetMaxNumber
+        {
+            get {
+                return this.cards.Max();
+            }
+        }
+
+
     }
 }
