6f12fe1c9352bb2c361fdad424785a3416aca1b8
すー <aaaiiiuuu35@gmail.com>
2019-05-27 23:22:34 +0900

    Add Card class

--- 5s/Poker/Poker/Card.cs
+++ 5s/Poker/Poker/Card.cs
@@ -4,7 +4,65 @@ using System.Text;
 
 namespace Poker
 {
-    class Card
+    public class Card
     {
+        public int Number { get; private set; }
+
+        public Card(int _num)
+        {
+            this.Number = _num;
+        }
+
+        public String GetString {
+            get { 
+                int num = (this.Number % 13);
+                String result = $"{num}";
+                switch (num)
+                {
+                    case 1:
+                        result = "A";
+                        break;
+                    case 10:
+                        result = "0";
+                        break;
+                    case 11:
+                        result = "J";
+                        break;
+                    case 12:
+                        result = "Q";
+                        break;
+                    case 0:
+                        result = "K";
+                        break;
+                }
+                return result;
+            }
+        }
+
+        public String GetSuitString {
+            get {
+                int num = (this.Number / 13);
+                String result = "";
+                switch (num)
+                {
+                    case 0:
+                        result = "S";
+                        break;
+                    case 1:
+                        result = "H";
+                        break;
+                    case 2:
+                        result = "D";
+                        break;
+                    case 3:
+                    case 4:
+                        result = "C";
+                        break;
+                }
+
+                return result + this.GetString;
+            }
+        }
+
     }
 }
