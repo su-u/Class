3b5fb4c5815873e73ea44470a8c0ca6cdb579b46
すー <aaaiiiuuu35@gmail.com>
2019-06-10 20:45:18 +0900

    Update :+1:

--- algorithm/Stack/Stack/Program.cs
+++ algorithm/Stack/Stack/Program.cs
@@ -7,44 +7,52 @@ namespace Stack
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             //input sample : 1 2 + 3 4 - *
             var line = Console.ReadLine()?.Split();
-            Stack<string> stack = new Stack<string>();
+            var stack = new Stack<string>();
 
-            foreach (var s in line)
+            foreach (var s in line ?? Enumerable.Empty<string>())
             {
                 stack.Push(s);
                 switch (stack.Peek())
                 {
                     case "+":
+                        {
+                            stack.Pop();
+                            var t1 = stack.Pop();
+                            var t2 = stack.Pop();
+                            Console.WriteLine($"{t2} + {t1}");
+                            stack.Push(Convert.ToString(int.Parse(t2) + int.Parse(t1)));
+                            break;
+                        }
+                    case "-":
+                    {
                         stack.Pop();
                         var t1 = stack.Pop();
                         var t2 = stack.Pop();
-                        Console.WriteLine($"{t2} + {t1}");
-                        stack.Push(Convert.ToString(int.Parse(t2) + int.Parse(t1)));
-                        break;
-                    case "-":
-                        stack.Pop();
-                        var t3 = stack.Pop();
-                        var t4 = stack.Pop();
-                        Console.WriteLine($"{t4} - {t3}");
-                        stack.Push(Convert.ToString(int.Parse(t4) - int.Parse(t3)));
+                        Console.WriteLine($"{t2} - {t1}");
+                        stack.Push(Convert.ToString(int.Parse(t2) - int.Parse(t1)));
+                    }
                         break;
                     case "*":
+                    {
                         stack.Pop();
-                        var t5 = stack.Pop();
-                        var t6 = stack.Pop();
-                        Console.WriteLine($"{t6} * {t5}");
-                        stack.Push(Convert.ToString(int.Parse(t6) * int.Parse(t5)));
+                        var t1 = stack.Pop();
+                        var t2 = stack.Pop();
+                        Console.WriteLine($"{t2} * {t1}");
+                        stack.Push(Convert.ToString(int.Parse(t2) * int.Parse(t1)));
+                    }
                         break;
                     case "/":
+                    {
                         stack.Pop();
-                        var t7 = stack.Pop();
-                        var t8 = stack.Pop();
-                        Console.WriteLine($"{t8} * {t7}");
-                        stack.Push(Convert.ToString(int.Parse(t8) * int.Parse(t7)));
+                        var t1 = stack.Pop();
+                        var t2 = stack.Pop();
+                        Console.WriteLine($"{t2} * {t1}");
+                        stack.Push(Convert.ToString(int.Parse(t2) * int.Parse(t1)));
+                    }
                         break;
                     default:
                         break;
@@ -52,7 +60,7 @@ namespace Stack
             }
 
             
-            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack?.Pop());
         }
     }
 }
