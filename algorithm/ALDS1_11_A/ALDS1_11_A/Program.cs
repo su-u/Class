e509e23cf8ad0e63a38ca576ede2aba903a952f8
すー <aaaiiiuuu35@gmail.com>
2019-06-10 22:27:53 +0900

    Add: Graph project

---
+++ algorithm/ALDS1_11_A/ALDS1_11_A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALDS1_11_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
