b346acb522ee419cbb1bdcc9de988f9aee7afb28
すー <aaaiiiuuu35@gmail.com>
2019-06-10 19:58:10 +0900

    Add : stackを利用した計算機

--- algorithm/Stack/Stack/Program.cs
+++ algorithm/Stack/Stack/Program.cs
@@ -9,14 +9,50 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            Stack<int> stack = new Stack<int>();
+            //input sample : 1 2 + 3 4 - *
+            var line = Console.ReadLine()?.Split();
+            Stack<string> stack = new Stack<string>();
 
-            stack.Push(1);
+            foreach (var s in line)
+            {
+                stack.Push(s);
+                switch (stack.Peek())
+                {
+                    case "+":
+                        stack.Pop();
+                        var t1 = stack.Pop();
+                        var t2 = stack.Pop();
+                        Console.WriteLine($"{t2} + {t1}");
+                        stack.Push(Convert.ToString(int.Parse(t2) + int.Parse(t1)));
+                        break;
+                    case "-":
+                        stack.Pop();
+                        var t3 = stack.Pop();
+                        var t4 = stack.Pop();
+                        Console.WriteLine($"{t4} - {t3}");
+                        stack.Push(Convert.ToString(int.Parse(t4) - int.Parse(t3)));
+                        break;
+                    case "*":
+                        stack.Pop();
+                        var t5 = stack.Pop();
+                        var t6 = stack.Pop();
+                        Console.WriteLine($"{t6} * {t5}");
+                        stack.Push(Convert.ToString(int.Parse(t6) * int.Parse(t5)));
+                        break;
+                    case "/":
+                        stack.Pop();
+                        var t7 = stack.Pop();
+                        var t8 = stack.Pop();
+                        Console.WriteLine($"{t8} * {t7}");
+                        stack.Push(Convert.ToString(int.Parse(t8) * int.Parse(t7)));
+                        break;
+                    default:
+                        break;
+                }
+            }
 
-            Console.WriteLine(stack.Peek());
             
             Console.WriteLine(stack.Pop());
-            Console.WriteLine(stack.Count);
         }
     }
 }
