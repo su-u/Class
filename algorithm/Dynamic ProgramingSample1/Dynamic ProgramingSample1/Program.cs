25d173af400471397318ae3cc2bd23a3b621ff4b
すー <aaaiiiuuu35@gmail.com>
2019-06-10 22:16:59 +0900

    Add: fibonacci

--- algorithm/Dynamic ProgramingSample1/Dynamic ProgramingSample1/Program.cs
+++ algorithm/Dynamic ProgramingSample1/Dynamic ProgramingSample1/Program.cs
@@ -4,9 +4,38 @@ namespace Dynamic_ProgramingSample1
 {
     class Program
     {
+        private static decimal count = 0m;
+
+        private static decimal[] memo;
+        //フィボナッチ数列
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const decimal f = 10m;
+            memo = new decimal[(int)f];
+            MakeFibonacci(f);
+            Console.WriteLine(memo[(int)f - 1]);
+            Console.WriteLine(count);
+        }
+
+
+        private static decimal Fibonacci(decimal n)
+        {
+            count++;
+            if (n == 0m || n == 1m) return memo[(int)n] = 1m;
+
+            if (memo?[(int) n] != 0) return memo[(int) n];
+            return memo[(int)n] = Fibonacci(n - 2m) + Fibonacci(n - 1m);
+        }
+
+        private static void MakeFibonacci(decimal n)
+        {
+            memo[0] = 1;
+            memo[1] = 1;
+
+            for (decimal i = 2; i < n; i++)
+            {
+                memo[(int) i] = memo[(int)i - 2] + memo[(int)i - 1];
+            }
         }
     }
 }
