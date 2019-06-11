75ba00cdce68875f50defdfe067e357bc17a7374
すー <aaaiiiuuu35@gmail.com>
2019-06-11 21:37:26 +0900

    Update Program.cs

--- algorithm/ALDS1_11_B/ALDS1_11_B/Program.cs
+++ algorithm/ALDS1_11_B/ALDS1_11_B/Program.cs
@@ -4,9 +4,10 @@ namespace ALDS1_11_B
 {
     class Program
     {
+        //負荷さ優先探索
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
