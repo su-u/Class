433db9a6125be218b098404950b9d2207c311a28
すー <aaaiiiuuu35@gmail.com>
2019-06-11 22:05:32 +0900

    Add: Road text file

--- algorithm/ALDS/ALDS/Program.cs
+++ algorithm/ALDS/ALDS/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ALDS
 {
@@ -7,7 +8,31 @@ namespace ALDS
         //深さ優先探索
         static void Main(string[] args)
         {
-            
+            var n = int.Parse(Console.ReadLine());
+
+            var graph = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var line = Console.ReadLine()?.Split().Select(int.Parse).ToArray();
+                for (int j = 2; j < line?.Length; j++)
+                {
+                    graph[line[0] - 1, line[j] - 1] = 1;
+
+                    //無向グラフの場合
+                    //graph[line[j] - 1, line[0] - 1] = 1;
+                }
+            }
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                for (int j = 0; j < graph.GetLength(1); j++)
+                {
+                    Console.Write($"{graph[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+
         }
     }
 }
