6d90b782c9dc2bf292380e3960626ab3e1cb98c8
すー <aaaiiiuuu35@gmail.com>
2019-06-11 22:35:18 +0900

    Update Program.cs

--- algorithm/ALDS/ALDS/Program.cs
+++ algorithm/ALDS/ALDS/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ALDS
 {
     class Program
     {
+
+        private static Stack<int> stack;
+        private static int[,] graph;
+        private static int[] state;
+
+
+
         //深さ優先探索
         static void Main(string[] args)
         {
             var n = int.Parse(Console.ReadLine());
 
-            var graph = new int[n, n];
+            graph = new int[n, n];
+            state = new int[n];
+
 
             for (int i = 0; i < n; i++)
             {
@@ -34,5 +44,22 @@ namespace ALDS
             }
 
         }
+
+        private static void Init()
+        {
+            for (int i = 0; i < state.Length; i++)
+            {
+                state[i] = 0;
+            }
+            
+        }
+
+        private static void Dts(int point)
+        {
+            stack.Push(point);
+            state[point] = 1;
+
+        }
+
     }
 }
