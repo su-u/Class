18150fd595af7879a63979c690fe3780a11b127f
すー <s1732087AP@s.chibakoudai.jp>
2019-06-21 14:42:38 +0900

    Update ICS.js

--- code/JavaScript/ICS/ICS.js
+++ code/JavaScript/ICS/ICS.js
@@ -1,3 +1,18 @@
 let Enumerable = require('linq');
+let Student = require('./Student')
 
-class 
+class Assignmment{
+    constructor(){
+        for (let index = 0; index < 123; index++) {
+            this.student[i] = new 
+            
+        }
+    }
+
+
+    Run(){
+
+    }
+
+
+}
