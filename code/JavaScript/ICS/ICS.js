cbfda3d3758119cbaf917af66a65a9c8b4d0dcc6
すー <aaaiiiuuu35@gmail.com>
2019-06-21 18:35:06 +0900

    Add: labo init

--- code/JavaScript/ICS/ICS.js
+++ code/JavaScript/ICS/ICS.js
@@ -1,23 +1,31 @@
-let Enumerable = require('linq');
-let Student = require('./Student')
+const Enumerable = require('linq');
+const Student = require('./Student')
+const Laboratory = require('./Laboratory')
 
 class Assignmment {
     constructor() {
+        this.students = [];
+        this.laboratories = [];
         for (let index = 0; index < 123; index++) {
             let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-            this.student[i] = new Student(index + 1, 3.3, list)
+            this.students[index] = new Student("test", index + 1, 3.3, list)
         }
 
         for (let index = 0; index < 7; index++) {
-            this.laboratory[i] = new laboratory(index + 1, "Test", 14)
+            this.laboratories[index] = new Laboratory(index + 1, "Test", 14)
 
         }
     }
 
 
     Run() {
-        
+        this.students.forEach(element => {
+            console.log(element.ToString)
+        });
     }
 
 
 }
+
+let app = new Assignmment();
+app.Run();
