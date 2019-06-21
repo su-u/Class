437c92294a66e9d5b97b9f950ced5dcb92f23a8d
すー <aaaiiiuuu35@gmail.com>
2019-06-21 18:18:52 +0900

    Update: constructor

--- code/JavaScript/ICS/ICS.js
+++ code/JavaScript/ICS/ICS.js
@@ -1,17 +1,22 @@
 let Enumerable = require('linq');
 let Student = require('./Student')
 
-class Assignmment{
-    constructor(){
+class Assignmment {
+    constructor() {
         for (let index = 0; index < 123; index++) {
-            this.student[i] = new 
-            
+            let list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            this.student[i] = new Student(index + 1, 3.3, list)
+        }
+
+        for (let index = 0; index < 7; index++) {
+            this.laboratory[i] = new laboratory(index + 1, "Test", 14)
+
         }
     }
 
 
-    Run(){
-
+    Run() {
+        
     }
 
 
