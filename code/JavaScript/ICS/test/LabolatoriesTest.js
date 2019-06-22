e595abcddf7cc5ec76b45313dac6c61fccecb607
すー <aaaiiiuuu35@gmail.com>
2019-06-22 14:11:45 +0900

    Update LabolatoriesTest.js

--- code/JavaScript/ICS/test/LabolatoriesTest.js
+++ code/JavaScript/ICS/test/LabolatoriesTest.js
@@ -5,16 +5,27 @@ const Enumerable = require("linq");
 
 
 describe("LaboratoryTest", function () {
-    let students = [];
-    for (let index = 0; index < 123; index++) {
-        let list = [10, 4, 10, 7, 1, 2, 9, 8, 1, 10];
-        students[index] = new Student("test", index + 1, 3.3, list);
-    }
-
-    laboratory = new Laboratory(1, "Test", 14);
+    it("Number", function () {
+        laboratory = new Laboratory(1, "Test", 14);
+        assert.equal(laboratory.Number, 1);
+    });
     it("DT", function () {
+        let list = [10, 4, 10, 7, 1, 2, 9, 8, 1, 10];
+        student = new Student("test", 1, 3.3, list);
+        laboratory = new Laboratory(1, "Test", 14);
+
+        laboratory.DT = student.GetSati(laboratory.Number);
+
+        assert.equal(laboratory.DT, 10);
+    });
+    it("DT-dtudents", function () {
+        let students = [];
+        for (let index = 0; index < 123; index++) {
+            let list = [10, 4, 10, 7, 1, 2, 9, 8, 1, 10];
+            students[index] = new Student("test", index + 1, 3.3, list);
+        }
+        laboratory = new Laboratory(1, "Test", 14);
         laboratory.DT = Enumerable.from(this.students).sum(x => x.GetSati(element.Number));
-        console.log(laboratory.DT);
         assert.equal(laboratory.DT, 1230);
     });
 })
