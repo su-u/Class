76d6cfa94fe27d077b0dea0c519ca09c58c51571
すー <aaaiiiuuu35@gmail.com>
2019-06-22 11:16:42 +0900

    Update StudentsTest.js

--- code/JavaScript/ICS/test/StudentsTest.js
+++ code/JavaScript/ICS/test/StudentsTest.js
@@ -1,8 +1,15 @@
-const assert = require('assert')
+const assert = require("assert");
+const Student = require("../Student");
 
-describe('StudentTest', function () {
-    it('Equality', function () {
-        assert(1 + 1 === 2)
-        assert.equal(1 + 1, 2)
+describe("StudentTest", function () {
+    let list = [4, 4, 10, 7, 1, 2, 9, 8, 1, 10];
+    let s = new Student("testName", 1, 2.5, list)
+    it("Gpa", function () {
+        assert(s.gpa === 2.5);
+        assert.equal(s.gpa, 2.5);
+    })
+    it("name", function () {
+        assert(s.Name === "testName");
+        assert.equal(s.Name, "testName");
     })
 })
