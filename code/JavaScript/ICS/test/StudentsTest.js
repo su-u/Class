eb346f2b2eebf6635753c0e347b30a5c78598ba0
すー <aaaiiiuuu35@gmail.com>
2019-06-26 21:34:05 +0900

    Update: 不必要なテストコード削除

--- code/JavaScript/ICS/test/StudentsTest.js
+++ code/JavaScript/ICS/test/StudentsTest.js
@@ -5,24 +5,19 @@ describe("StudentTest", function () {
     let list = [4, 4, 10, 7, 1, 2, 9, 8, 1, 10];
     let s = new Student("testName", 1, 2.5, list)
     it("Gpa", function () {
-        assert(s.gpa === 2.5);
         assert.equal(s.gpa, 2.5);
     });
     it("name", function () {
-        assert(s.Name === "testName");
         assert.equal(s.Name, "testName");
     });
     it("satisfaction", function () {
-        assert(s.SatisfactionToString === " 3,10 10,10 7,9 8,8 4,7 1,4 2,4 6,2 5,1 9,1");
         assert.equal(s.SatisfactionToString, " 3,10 10,10 7,9 8,8 4,7 1,4 2,4 6,2 5,1 9,1");
     });
     it("satisfactionSum", function () {
-        assert(s.SatisfactionSum === 56);
         assert.equal(s.SatisfactionSum, 56);
     });
     it("get satifaction", function () {
         const n = s.GetSati(1);
-        assert(n === 4);
         assert.equal(n, 4);
     });
     it("SumStudentsSatisfaction", function () {
