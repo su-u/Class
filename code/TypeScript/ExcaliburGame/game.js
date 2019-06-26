c22d4f6eec51df4f0306202ec3c7fd4c4b8f5fee
すー <s1732087AP@s.chibakoudai.jp>
2019-06-26 11:03:51 +0900

    Update game.js

---
+++ code/TypeScript/ExcaliburGame/game.js
@@ -0,0 +1,13 @@
+// game.js
+
+// Create an instance of the engine.
+// I'm specifying that the game be 800 pixels wide by 600 pixels tall.
+// If no dimensions are specified the game will be fullscreen.
+var game = new ex.Engine({
+    width: 800,
+    height: 600,
+  })
+  // todo build awesome game here
+  
+  // Start the engine to begin the game.
+  game.start()
