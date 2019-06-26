3795994557c686f478f3d98862e4de04c73ff9f6
すー <s1732087AP@s.chibakoudai.jp>
2019-06-26 11:08:45 +0900

    Update game.js

--- code/TypeScript/ExcaliburGame/game.js
+++ code/TypeScript/ExcaliburGame/game.js
@@ -1,13 +1,26 @@
 // game.js
 
 // Create an instance of the engine.
-// I'm specifying that the game be 800 pixels wide by 600 pixels tall.
-// If no dimensions are specified the game will be fullscreen.
 var game = new ex.Engine({
     width: 800,
     height: 600,
   })
-  // todo build awesome game here
+  
+  // Create an actor with x position of 150px,
+  // y position of 40px from the bottom of the screen,
+  // width of 200px, height and a height of 20px
+  var paddle = new ex.Actor(150, game.drawHeight - 40, 200, 20)
+  
+  // Let's give it some color with one of the predefined
+  // color constants
+  paddle.color = ex.Color.Chartreuse
+  
+  // Make sure the paddle can partipate in collisions, by default excalibur actors do not collide
+  paddle.collisionType = ex.CollisionType.Fixed
+  
+  // `game.add` is the same as calling
+  // `game.currentScene.add`
+  game.add(paddle)
   
   // Start the engine to begin the game.
   game.start()
