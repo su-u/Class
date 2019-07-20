f438469b350c4bf9f0c4598e1753e467f7957e73
すー <aaaiiiuuu35@gmail.com>
2019-07-20 12:49:56 +0900

    AC: D106

--- code/Paiza/D106/D106/Program.cs
+++ code/Paiza/D106/D106/Program.cs
@@ -5,77 +5,132 @@ using System.Linq;
 using System.Text;
 using static System.Console;
 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using static System.Math;
+using static System.Console;
+
+using static CPL.Input.IO;
+using CPL.Input;
+using CPL.Collections;
+using CPL.String;
+
+
+namespace CPL
+{
+    namespace Input
+    {
+        public static class IO
+        {
+            public static T RL<T>()
+            {
+                return Console.ReadLine().Trim().TryParse<T>();
+            }
+
+            public static List<T> RLL<T>()
+            {
+                return Console.ReadLine().TrySplitParseToList<T>();
+            }
+
+            public static void WL(string s)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        public static class InputEx
+        {
+
+            public static T TryParse<T>(this string input)
+            {
+                try
+                {
+                    var converter = TypeDescriptor.GetConverter(typeof(T));
+                    return (T)converter.ConvertFromString(input);
+                }
+                catch
+                {
+                    throw new InvalidCastException(typeof(T) + " is not supported.");
+                }
+            }
+
+            public static List<T> TrySplitParseToList<T>(this string input, char separator = ' ')
+            {
+                return input.Split(separator).Select(TryParse<T>).ToList();
+            }
+
+            public static List<T> ReadLineOne<T>(int n)
+            {
+                return Enumerable.Range(1, n).Select(i => Console.ReadLine().TryParse<T>()).ToList();
+            }
+        }
+    }
+
+    namespace Collections
+    {
+        public static class CollectionsEx
+        {
+            public static List<Tuple<T, int>> DuplicateCount<T>(this IEnumerable<T> list)
+            {
+                return list
+                    .GroupBy(i => i)
+                    .Select(g => Tuple.Create(g.Key, g.Count()))
+                    .ToList();
+            }
+
+            public static List<Tuple<T, int>> DuplicateOrderByDescending<T>(this IEnumerable<Tuple<T, int>> list)
+            {
+                return list.OrderByDescending(x => x.Item2).ToList();
+            }
+
+            public static List<T> ListSwap<T>(this List<T> list, int index1, int index2)
+            {
+                var t = list[index1];
+                list[index1] = list[index2];
+                list[index2] = t;
+                return list;
+            }
+
+            public static void PrintAll<T>(this IEnumerable<T> list)
+            {
+                foreach (var i in list)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+
+    namespace String
+    {
+        public static class StringEx
+        {
+            public static string Reversed(this string s)
+            {
+                return string.Join("", s.Reverse());
+            }
+        }
+    }
+
+
+}
+
+
 namespace D106
 {
     static class Program
     {
         static void Main(string[] args)
         {
+            var n = RL<int>();
+            var m = RL<int>();
 
-        }
-
-        public static T TryParse<T>(this String input)
-        {
-            try
-            {
-                var converter = TypeDescriptor.GetConverter(typeof(T));
-                if (converter != null)
-                {
-                    return (T)converter.ConvertFromString(input);
-                }
-                else
-                {
-                    throw new InvalidCastException("");
-                }
-            }
-            catch
-            {
-                throw new InvalidCastException(typeof(T) + " is not supported.");
-            }
+            WL($"{n % m}");
         }
 
-        public static List<T> SplitTryParseToList<T>(this String input)
-        {
-            return input.Split().Select(n => n.TryParse<T>()).ToList();
-        }
-
-        public static List<T> ListSwap<T>(this List<T> list, Int32 index1, Int32 index2)
-        {
-            var t = list[index1];
-            list[index1] = list[index2];
-            list[index2] = t;
-            return list;
-        }
-
-        public static List<Tuple<T, int>> DuplicateCount<T>(this IEnumerable<T> list)
-        {
-            return list
-                .GroupBy(i => i)
-                .Where(g => g.Any())
-                .Select(g => Tuple.Create(g.Key, g.Count()))
-                .ToList();
-        }
-        public static List<Tuple<T, int>> DuplicateSort<T>(this IEnumerable<Tuple<T, int>> list)
-        {
-            return list.OrderByDescending((x) => x.Item2).ToList();
-        }
-        public static List<T> ReadLineOne<T>(int n)
-        {
-            var list = new List<T>();
-            foreach (var i in Enumerable.Range(1, n))
-            {
-                list.Add(Console.ReadLine().TryParse<T>());
-            }
-            return list;
-        }
-        public static void PrintAll<T>(this IEnumerable<T> list)
-        {
-            foreach (var i in list)
-            {
-                Console.Write($"{i} ");
-            }
-
-            Console.WriteLine();
-        }
+        
     }
 }
