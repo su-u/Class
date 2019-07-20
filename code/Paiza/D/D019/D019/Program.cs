fe3d9263c7d0cc1b1cf95662c8f3938af051824e
すー <aaaiiiuuu35@gmail.com>
2019-07-20 13:55:25 +0900

    AC: D019

--- code/Paiza/D/D019/D019/Program.cs
+++ code/Paiza/D/D019/D019/Program.cs
@@ -11,7 +11,8 @@ namespace D019
     {
         static void Main(string[] args)
         {
-
+            var line = ReadLine().Trim().Split().ToList();
+            WriteLine(line[0][line[1].TryParse<int>() - 1]);
         }
 
         public static T TryParse<T>(this String input)
