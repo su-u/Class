fa36fbc6a3cd999be592de4b16eb484141b8e9d2
すー <aaaiiiuuu35@gmail.com>
2019-08-14 17:55:40 +0900

    memo-1

---
+++ code/Python/koneta_memo.py
@@ -0,0 +1,28 @@
+#%%
+
+sum = 1_0_0_0
+sum2 = 10_00_00_00
+sum3 = 100_00
+
+sum = sum2 + sum
+print(sum)
+
+print(f'{sum:,}')
+
+#%%
+condition = True
+
+if condition:
+    x = 1
+else:
+    x = 2
+
+print(x)
+
+#%%
+condition = True
+x = 3 if condition else 4
+print(x)
+
+
+#%%
